d9262b6935dd75cfa9ddc58ade02962ee6344aac
Swann <swann.rouanet@gmail.com>
2024-08-08 01:31:46 +0200

    feat: replace followers by repositories count in navbar profile

--- src/components/navigation/navbar-profile.tsx
+++ src/components/navigation/navbar-profile.tsx
@@ -1,12 +1,15 @@
-'use client';
+"use client";
 
 import useSWR from "swr";
 
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Skeleton } from "@/components/ui/skeleton";
-import { PersonIcon } from "@radix-ui/react-icons";
-
+import { RepositoryIcon } from "@/components/icons/repository-icon";
 
 export default function NavbarProfile() {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
@@ -15,11 +18,19 @@ export default function NavbarProfile() {
   return (
     <HoverCard>
       <HoverCardTrigger asChild className="hover:cursor-pointer">
-        <a href="https://github.com/hezaerd" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/hezaerd"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <div className="flex items-center gap-4">
             <div className="text-lg font-semibold">Hezaerd</div>
             <Avatar className="h-9 w-9">
-              <AvatarImage src="https://github.com/hezaerd.png" alt="Hezaerd" className="mx-auto" />
+              <AvatarImage
+                src="https://github.com/hezaerd.png"
+                alt="Hezaerd"
+                className="mx-auto"
+              />
               <AvatarFallback>
                 <Skeleton className="h-9 w-9 rounded-full" />
               </AvatarFallback>
@@ -31,30 +42,47 @@ export default function NavbarProfile() {
         <div className="flex justify-between space-x-4">
           <div className="self-center">
             <Avatar className="h-16 w-16">
-              <a href="https://github.com/hezaerd" target="_blank" rel="noopener noreferrer">
-                <AvatarImage src="https://github.com/hezaerd.png" alt="Hezaerd" className="mx-auto" />
+              <a
+                href="https://github.com/hezaerd"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AvatarImage
+                  src="https://github.com/hezaerd.png"
+                  alt="Hezaerd"
+                  className="mx-auto"
+                />
               </a>
               <AvatarFallback>
                 <Skeleton className="h-16 w-16 rounded-full" />
               </AvatarFallback>
             </Avatar>
           </div>
-          <div className="space-y-1 items-center">
-            <a href="https://github.com/hezaerd" target="_blank" rel="noopener noreferrer">
+          <div className="items-center space-y-1">
+            <a
+              href="https://github.com/hezaerd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <h4 className="text-sm font-semibold">@hezaerd</h4>
             </a>
-            <p className="text-sm">
-              {data ? data.bio : "Loading..."}
-            </p>
+            <p className="text-sm">{data ? data.bio : "Loading..."}</p>
             <div className="flex items-center pt-2">
-              <PersonIcon className="mr-2 h-4 w-4 opacity-70" />{" "}
-              <span className="text-xs text-muted-foreground">
-                {data ? data.followers : "..."} followers
-              </span>
+              <a
+                href="https://github.com/Hezaerd?tab=repositories"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-1"
+              >
+                <RepositoryIcon color="#a1a1aa" />
+                <span className="text-xs text-muted-foreground">
+                  {data ? data.public_repos : "..."} repositories
+                </span>
+              </a>
             </div>
           </div>
         </div>
       </HoverCardContent>
     </HoverCard>
-  )
+  );
 }
