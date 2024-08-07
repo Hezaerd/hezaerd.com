5b1b71fe05f6f1c5184a89a40e66980ecc67de1b
Swann <swann.rouanet@gmail.com>
2024-08-08 01:32:20 +0200

    feat: aurora background hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,11 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
           {children}
         </ThemeProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import { AuroraBackground } from "@/components/ui/aurora-background";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
+import { motion } from "framer-motion";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello</div>
+      <AuroraBackground>
+        <motion.div
+          initial={{ opacity: 0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+        >
+          <div className="text-center text-3xl font-bold dark:text-primary md:text-7xl">
+            Hello I'm Hezaerd
+          </div>
+          <div className="text-center text-xl dark:text-primary md:text-3xl">
+            I'm a full stack developer
+          </div>
+        </motion.div>
+      </AuroraBackground>
     </main>
   );
 }
