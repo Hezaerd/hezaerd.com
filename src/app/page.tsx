ede5046103bcec454bedb09e9bf8c71a77d14272
Swann <swann.rouanet@gmail.com>
2024-08-08 15:27:14 +0200

    feat: add button to projects in hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
             duration: 0.8,
             ease: "easeInOut",
           }}
+          className="relative flex flex-col items-center justify-center gap-4 px-4"
         >
           <div className="text-center text-3xl font-bold dark:text-primary md:text-7xl">
             Hello I'm Hezaerd
@@ -23,6 +24,15 @@ export default function Home() {
           <div className="text-center text-xl dark:text-primary md:text-3xl">
             I'm a full stack developer
           </div>
+          <HoverBorderGradient
+            className="w-fit"
+            as="button"
+            containerClassName="rounded-full"
+          >
+            <a href="/projects" className="text-lg font-semibold">
+              Checkout my projects
+            </a>
+          </HoverBorderGradient>
         </motion.div>
       </AuroraBackground>
     </main>
