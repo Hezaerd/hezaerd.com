b01ab92408daed5c458fc51247f7c7385c7dc74a
Swann <swann.rouanet@gmail.com>
2024-09-20 17:32:20 +0200

    feat: remove TracingBeam from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import AboutMe from "@/components/home/about-me";
 import HeroLanding from "@/components/home/hero-landing";
 import ProjectsBento from "@/components/home/projects-bento";
-import { TracingBeam } from "@/components/ui/tracing-beam";
 
 export default function Home() {
   return (
     <main>
       <HeroLanding />
-      <TracingBeam className="px-6">
-        <AboutMe />
-        <ProjectsBento />
-      </TracingBeam>
+      <AboutMe />
+      <ProjectsBento />
     </main>
   );
 }
