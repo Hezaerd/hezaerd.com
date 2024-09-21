fddd5042d66a5f7084cd1bf8c61405a510ec5018
Swann <swann.rouanet@gmail.com>
2024-09-21 20:57:37 +0200

    feat: (re)add TracingBeam to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import AboutMe from "@/components/home/about-me";
 import HeroLanding from "@/components/home/hero-landing";
 import ProjectsBento from "@/components/home/projects-bento";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 
 export default function Home() {
   return (
     <main>
       <HeroLanding />
-      <AboutMe />
-      <ProjectsBento />
+      <TracingBeam>
+        <AboutMe />
+        <ProjectsBento />
+      </TracingBeam>
     </main>
   );
 }
