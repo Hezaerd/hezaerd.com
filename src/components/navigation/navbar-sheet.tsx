f9cc547e45a02a32de122ce70f5f64824c591a15
Swann <swann.rouanet@gmail.com>
2024-10-03 21:44:31 +0200

    feat: add stats navlink to navbar sheet

--- src/components/navigation/navbar-sheet.tsx
+++ src/components/navigation/navbar-sheet.tsx
@@ -1,20 +1,25 @@
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Button } from "../ui/button";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
 import { NavLink } from "./navlink";
 
 export default function NavbarSheet() {
   return (
     <Sheet>
-
       <SheetTrigger asChild>
         <Button variant="ghost" size="icon" className="md:hidden">
           <HamburgerMenuIcon className="h-5 w-5" />
-            <span className="sr-only">Open navigation</span>
+          <span className="sr-only">Open navigation</span>
         </Button>
       </SheetTrigger>
       <SheetContent side="right" className="w-full max-w-xs bg-background p-6">
-
         <SheetHeader>
           <SheetTitle>Menu</SheetTitle>
         </SheetHeader>
@@ -26,10 +31,11 @@ export default function NavbarSheet() {
         <nav className="grid gap-4">
           <NavLink name="Home" href="/" />
           <NavLink name="About" href="/about" />
+          <NavLink name="Stats" href="/stats" />
           <NavLink name="Projects" href="/projects" />
           <NavLink name="Skills" href="/skills" />
         </nav>
       </SheetContent>
     </Sheet>
-  )
+  );
 }
