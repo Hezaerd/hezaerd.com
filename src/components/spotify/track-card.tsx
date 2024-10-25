10b9814d0956717f708111eabe58eb1308cb1579
Swann <swann.rouanet@gmail.com>
2024-10-25 16:13:22 +0200

    feat: TrackCard skeleton, TrackCard hover, inline loading logic, better error handling

--- src/components/spotify/track-card.tsx
+++ src/components/spotify/track-card.tsx
@@ -1,29 +1,49 @@
 import Image from "next/image";
 
 import { ISpotifyTrack } from "@/interfaces/spotify";
+import { Skeleton } from "@/components/ui/skeleton";
+import { motion } from "framer-motion";
 
 export const SpotifyTrackCard = ({ track }: { track: ISpotifyTrack }) => {
   return (
-    <div className="flex w-full items-center space-x-4">
-      <Image
-        src={track.album.images[0].url}
-        alt={track.name}
-        width={64}
-        height={64}
-        className="rounded-md"
-      />
-      <div className="flex-1">
-        <h2 className="font-bold">
-          {track.name.replace(/\s*\(.*?\)\s*/g, "")}
-        </h2>
-        <p className="text-sm text-primary/80">
-          {track.artists.length > 3
-            ? `${track.artists
-                .slice(0, 3)
-                .map((artist) => artist.name)
-                .join(", ")}...`
-            : track.artists.map((artist) => artist.name).join(", ")}
-        </p>
+    <motion.div
+      className="w-80 rounded-lg p-1.5 transition-colors hover:bg-gray-50/5"
+      whileHover={{ scale: 1.02 }}
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
+    >
+      <div className="flex w-full items-center space-x-4 rounded-md">
+        <Image
+          src={track.album.images[0].url}
+          alt={track.name}
+          width={64}
+          height={64}
+          className="rounded-md"
+        />
+        <div className="flex-1 overflow-hidden">
+          <h2 className="truncate font-bold">
+            {track.name.replace(/\s*\(.*?\)\s*/g, "")}
+          </h2>
+          <p className="truncate text-sm text-primary/80">
+            {track.artists.map((artist) => artist.name).join(", ")}
+          </p>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export const SpotifyTrackCardSkeleton = () => {
+  return (
+    <div className="rounded-lg p-1.5">
+      <div className="flex w-full items-center space-x-4">
+        {/* Skeleton for the album image */}
+        <Skeleton className="h-16 w-16 rounded-md" />
+
+        {/* Skeleton for track name and artist */}
+        <div className="flex-1 space-y-2">
+          <Skeleton className="h-5 w-3/4" /> {/* Simulates track name */}
+          <Skeleton className="h-4 w-1/2" /> {/* Simulates artist names */}
+        </div>
       </div>
     </div>
   );
