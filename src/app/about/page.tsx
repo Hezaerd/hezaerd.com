1126f45641b3c566e1579bd128f50454c7415d55
Swann <swann.rouanet@gmail.com>
2024-10-29 15:34:10 +0100

    fix: replace ' by &apos;

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -149,9 +149,9 @@ export default function AboutPage() {
       <h1 className="text-4xl font-bold text-white">About Me</h1>
       <p className="mt-4 text-lg text-neutral-300">
         I&apos;m a passionate game developer currently studying Game Engine
-        Programming at Isart Digital Paris. I'm eager to enhance my skills and
-        learn more about the industry, and I'm actively seeking an internship at
-        a game studio.
+        Programming at Isart Digital Paris. I&apos;m eager to enhance my skills
+        and learn more about the industry, and I&apos;m actively seeking an
+        internship at a game studio.
       </p>
 
       <Timeline config={tlConfig} data={data} />
