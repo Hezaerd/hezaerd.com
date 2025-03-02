61fb917f42b05dfddfac82e388264bec410f0668
Swann <swann.rouanet@gmail.com>
2025-03-02 03:46:56 +0100

    refact: nuke down project to restart from scratch

--- src/app/api/spotify/top-artists/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { topArtists } from "@/lib/spotify";
-
-export async function GET(req: NextRequest) {
-  const data = await topArtists();
-
-  return NextResponse.json(data);
-}
--- src/app/api/spotify/top-tracks/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { topTracks } from "@/lib/spotify";
-
-export async function GET(req: NextRequest) {
-  const data = await topTracks();
-
-  return NextResponse.json(data);
-}
