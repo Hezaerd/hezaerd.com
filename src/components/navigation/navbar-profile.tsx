ad2860af1528e989d05c539ff7ddf23c2e4cf1ee
Swann <swann.rouanet@gmail.com>
2025-04-13 00:55:05 +0200

    feat: update navbar

--- src/components/navigation/navbar-profile.tsx
+++ src/components/navigation/navbar-profile.tsx
@@ -25,24 +25,16 @@ export default function NavbarProfile() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <div className="flex items-center gap-2">
-            <SparklesText
-              text="@hezaerd"
-              sparklesCount={2}
-              colors={{ first: "#c084fc", second: "#c026d3" }}
-              className="text-lg font-semibold"
+          <Avatar className="h-9 w-9">
+            <AvatarImage
+              src="https://github.com/hezaerd.png"
+              alt="Hezaerd"
+              className="mx-auto"
             />
-            <Avatar className="h-9 w-9">
-              <AvatarImage
-                src="https://github.com/hezaerd.png"
-                alt="Hezaerd"
-                className="mx-auto"
-              />
-              <AvatarFallback>
-                <Skeleton className="h-9 w-9 rounded-full" />
-              </AvatarFallback>
-            </Avatar>
-          </div>
+            <AvatarFallback>
+              <Skeleton className="h-9 w-9 rounded-full" />
+            </AvatarFallback>
+          </Avatar>
         </a>
       </HoverCardTrigger>
       <HoverCardContent className="w-80">
--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { NavLink } from "@/components/navigation/navlink";
 import NavbarSheet from "./navbar-sheet";
 import NavbarProfile from "./navbar-profile";
@@ -6,27 +5,20 @@ import NavbarProfile from "./navbar-profile";
 export default function Navbar() {
   return (
     <div className="fixed left-0 top-0 z-50 w-full border-b border-muted/50 bg-background/60 shadow-lg backdrop-blur-lg">
-      <div className="container relative flex h-16 items-center justify-center px-4 md:px-6">
-        <Link
-          href={"/"}
-          prefetch={false}
-          className="absolute left-4 flex items-center gap-4 md:left-6"
-        >
-          <span className="text-lg font-semibold">Portfolio</span>
-        </Link>
-
+      <div className="container relative flex h-16 items-center justify-between px-4 md:px-6">
         <div className="flex items-center gap-4">
-          <nav className="hidden items-center gap-4 md:flex">
-            <NavLink name="Home" href="/" />
-            <NavLink name="Stats" href="/stats" />
-            <NavLink name="Projects" href="/projects" />
-            <NavLink name="Skills" href="/skills" />
-          </nav>
+          <NavbarProfile />
         </div>
 
-        <div className="absolute right-4 flex items-center gap-4 md:right-6">
+        <nav className="hidden items-center gap-6 md:flex">
+          <NavLink name="Home" href="/" />
+          <NavLink name="Stats" href="/stats" />
+          <NavLink name="Projects" href="/projects" />
+        </nav>
+
+        <div className="flex items-center gap-4">
+          <span className="text-lg font-semibold">Portfolio</span>
           <NavbarSheet />
-          <NavbarProfile />
         </div>
       </div>
     </div>
