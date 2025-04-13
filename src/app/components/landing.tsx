2775d4c9cdecbc23d8fddeb1579779d7ea24c996
Swann <swann.rouanet@gmail.com>
2025-04-13 05:44:37 +0200

    feat: make landing text gradient

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -1,22 +1,11 @@
 "use client";
 
 import Link from "next/link";
-import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { SendHorizontal } from "lucide-react";
 import { AuroraBackground } from "@/components/ui/aurora-background";
-
-const typeSequence = [
-  "also known as Swann",
-  2000,
-  "an engine developer",
-  1000,
-  "a gameplay Developer",
-  1000,
-  "a software engineer",
-  1000,
-];
+import { AuroraText } from "@/components/text/aurora-text";
 
 export function Landing() {
   return (
@@ -27,33 +16,29 @@ export function Landing() {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6 }}
-            className="mb-4 text-3xl font-bold text-primary md:text-7xl"
+            className="mb-4 text-2xl font-semibold text-primary sm:text-3xl md:text-5xl lg:text-7xl"
           >
-            Hello I&apos;m Hezaerd
+            <span className="inline-flex items-center">
+              Hello I&apos;m&nbsp;
+              <AuroraText
+                speed={0.5}
+                colors={["#FF0080", "#7928CA", "#0070F3", "#38bdf8"]}
+              >
+                Hezaerd
+              </AuroraText>
+              <span className="sr-only">Hezaerd</span>
+            </span>
           </motion.h1>
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.5 }}
-            className="text-xl text-primary md:text-3xl"
+            className="text-lg text-primary sm:text-xl md:text-2xl lg:text-3xl"
           >
-            <span className="inline-flex items-center">
-              I&apos;m&nbsp;
-              <span className="bg-gradient-to-r from-primary-gradient to-secondary-gradient bg-clip-text text-transparent">
-                <TypeAnimation
-                  sequence={typeSequence}
-                  wrapper="span"
-                  speed={55}
-                  repeat={Infinity}
-                  cursor={false}
-                  style={{ whiteSpace: "nowrap" }}
-                  preRenderFirstString={true}
-                />
-              </span>
-              <span className="sr-only">
-                known as Swann, an engine developer, a gameplay Developer and a
-                software engineer
-              </span>
+            <span className="inline-flex items-center text-primary/75">
+              a&nbsp;
+              <span className="underline">software engineer</span>
+              <span className="sr-only">software engineer</span>
             </span>
           </motion.div>
 
