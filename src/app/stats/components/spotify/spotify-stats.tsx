767b86ee8eb48f5a07def72737dd804dad00fa45
Swann <swann.rouanet@gmail.com>
2025-04-17 16:05:11 +0200

    feat: add spotify currently playing

--- src/app/stats/components/spotify/spotify-stats.tsx
+++ src/app/stats/components/spotify/spotify-stats.tsx
@@ -3,15 +3,16 @@
 import TopArtists from "@/app/stats/components/spotify/top-artists";
 import TopTracks from "@/app/stats/components/spotify/top-tracks";
 import RecentlyPlayed from "@/app/stats/components/spotify/recently-played";
+import { CurrentlyPlaying } from "@/app/stats/components/spotify/currently-playing";
 
 export default function SpotifyStats() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="container mx-auto px-4">
-        <h1 className="mb-4 text-center text-3xl font-bold">Spotify Stats</h1>
-        <p className="mb-8 text-center text-muted-foreground">
-          A look at my current Spotify listening habits
-        </p>
+        <div className="mb-8 flex flex-col items-center justify-center gap-2">
+          <h1 className="text-center text-3xl font-bold">Spotify Stats</h1>
+          <CurrentlyPlaying />
+        </div>
 
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           <div className="flex flex-col">
