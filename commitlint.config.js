fad005959192f2e3890eadfb35f5c8d081d97731
Swann <swann.rouanet@gmail.com>
2025-08-24 15:38:35 +0200

    feat(root): create turborepo project
    - Setup Husky & Commitlint
    - Setup biomejs

---
+++ commitlint.config.js
@@ -0,0 +1,35 @@
+export default {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"feat", // New feature
+				"fix", // Bug fix
+				"docs", // Documentation changes
+				"style", // Code style changes (formatting, missing semicolons, etc)
+				"refactor", // Code refactoring
+				"perf", // Performance improvements
+				"test", // Adding or updating tests
+				"chore", // Build process, tooling changes
+				"ci", // CI/CD changes
+				"revert", // Revert previous commit
+			],
+		],
+		"scope-enum": [
+			2,
+			"always",
+			[
+				"root",
+                "portfolio",
+                "mods"
+			],
+		],
+		"scope-empty": [2, "never"],
+		"subject-case": [2, "always", "lower-case"],
+		"subject-empty": [2, "never"],
+		"subject-full-stop": [2, "never", "."],
+		"header-max-length": [2, "always", 72],
+	},
+};
