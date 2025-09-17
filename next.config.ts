8199a2bda807e4f249ba3fdd4eaccc0465a427d9
Hezaerd <hezaerd@hezaerd.com>
2025-09-17 17:15:14 +0200

    chore: add experimental package import optimizations for Radix UI and motion

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	experimental: {
+        optimizePackageImports: [
+            '@radix-ui/react-dialog',
+            '@radix-ui/react-label',
+            '@radix-ui/react-navigation-menu',
+            '@radix-ui/react-progress',
+            '@radix-ui/react-slot',
+            '@radix-ui/react-tabs',
+            'sonner',
+            'motion',
+        ]
+    }
 };
 
 export default nextConfig;
